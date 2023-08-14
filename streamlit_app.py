2368d57682ff3096fbc727d606a560b8aecf94e9
snowflake-ashok28 <142199025+snowflake-ashok28@users.noreply.github.com>
2023-08-14 11:00:15 +0530

    Menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My First Streamlit APP')
+streamlit.header('Breakfast menu')
+
+streamlit.text('Omega3 & oat meal')
+streamlit.text('Idly and Sambar')
+streamlit.text('Chapathi and curry')
